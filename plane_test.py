dd821bdb04722ccb643058d16ab3be3b9889945d
av-mido <av-mido@users.noreply.github.com>
2017-02-18 17:24:04 +0400

    plane_test.py

---
+++ plane_test.py
@@ -0,0 +1,6 @@
+from plane import Plane
+from vector import Vector
+p1 = Plane(Vector([-0.412, 3.806, 0.728]), -3.46)
+p2 = Plane(Vector([1.03, -9.515, -1.82]), 8.65)
+print("planes are parallel? {}".format(p1.is_parallel_to(p2)))
+print("planes are equal? {}".format(p1 == p2))
