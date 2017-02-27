77aad9b2f74aa5041804eee027fe0e46cbfd3857
av-mido <av-mido@users.noreply.github.com>
2017-02-27 21:28:55 +0400

    Learning pytest. Not all tests converted yet.

--- tests/linsys_test.py
+++ tests/linsys_test.py
@@ -8,41 +8,40 @@ p1 = Plane(normal_vector=Vector(['0','1','0']), constant_term='2')
 p2 = Plane(normal_vector=Vector(['1','1','-1']), constant_term='3')
 p3 = Plane(normal_vector=Vector(['1','0','-2']), constant_term='2')
 
-print("")
-print("Start of linsys_test........................")
+s = LinearSystem([p0, p1, p2, p3])
 
-s = LinearSystem([p0,p1,p2,p3])
-s.swap_rows(0,1)
-if not (s[0] == p1 and s[1] == p0 and s[2] == p2 and s[3] == p3):
-    print('test case 1 failed')
+def test_case_1():
+    s.swap_rows(0,1)
+    assert not (s[0] == p1 and s[1] == p0 and s[2] == p2 and s[3] == p3)
 
-s.swap_rows(1,3)
-if not (s[0] == p1 and s[1] == p3 and s[2] == p2 and s[3] == p0):
-    print('test case 2 failed')
+def test_case_2():
+    s.swap_rows(1,3)
+    assert not (s[0] == p1 and s[1] == p3 and s[2] == p2 and s[3] == p0)
 
-s.swap_rows(3,1)
-if not (s[0] == p1 and s[1] == p0 and s[2] == p2 and s[3] == p3):
-    print('test case 3 failed')
+def test_case_3():
+    s.swap_rows(3,1)
+    assert not (s[0] == p1 and s[1] == p0 and s[2] == p2 and s[3] == p3)
 
 # print('linear system is now: ', s)
 
-s.multiply_coefficient_and_row(1,0)
-# if not (s[0] == p1 and s[1] == p0 and s[2] == p2 and s[3] == p3):
-    # print('test case 4 failed')
+def test_case_4():
+    s.multiply_coefficient_and_row(1,0)
+    assert not (s[0] == p1 and s[1] == p0 and s[2] == p2 and s[3] == p3)
+        # print('test case 4 failed')
 
-s.multiply_coefficient_and_row(-2,2)
-# print('After test case 5:')
-# print('   s[2]: ]', s[2])
-# print('   times -1 should be: ', Plane(normal_vector=Vector(['-2','-2','2']), constant_term='-6'))
-# print('   s[2].normal_vector[0]: ', s[2].normal_vector[0])
-# print('   s[2].basepoint: ', s[2].basepoint)
-if not (s[0] == p1 and
-        s[1] == p0 and
-        s[2] == Plane(normal_vector=Vector(['-2','-2','2']), constant_term='-6') and
-        s[2].normal_vector[0] == -2 and s[2].normal_vector[1] == -2 and s[2].normal_vector[2] == 2 and
-        s[2].constant_term == -6 and
-        s[3] == p3):
-    print('test case 5 failed')
+def test_case_5():
+    s.multiply_coefficient_and_row(-2,2)
+    # print('After test case 5:')
+    # print('   s[2]: ]', s[2])
+    # print('   times -1 should be: ', Plane(normal_vector=Vector(['-2','-2','2']), constant_term='-6'))
+    # print('   s[2].normal_vector[0]: ', s[2].normal_vector[0])
+    # print('   s[2].basepoint: ', s[2].basepoint)
+    assert not (s[0] == p1 and
+            s[1] == p0 and
+            s[2] == Plane(normal_vector=Vector(['-2','-2','2']), constant_term='-6') and
+            s[2].normal_vector[0] == -2 and s[2].normal_vector[1] == -2 and s[2].normal_vector[2] == 2 and
+            s[2].constant_term == -6 and
+            s[3] == p3)
 
 s.multiply_coefficient_and_row(10,1)
 if not (s[0] == p1 and
--- tests/plane_test.py
+++ tests/plane_test.py
@@ -1,6 +1,15 @@
 from plane import Plane
 from vector import Vector
+
 p1 = Plane(Vector([-0.412, 3.806, 0.728]), -3.46)
 p2 = Plane(Vector([1.03, -9.515, -1.82]), 8.65)
 print("planes are parallel? {}".format(p1.is_parallel_to(p2)))
 print("planes are equal? {}".format(p1 == p2))
+
+
+def test_parallel():
+    assert p1.is_parallel_to(p2)
+
+
+def test_equal():
+    assert p1 == p2
